c28a701510a14c7b27adf86b1a85ac21c6547d68
vinicius <vmm_7@hotmail.com>
2022-07-03 12:10:13 -0300

    Configuração básica do compilador tsconfig.json

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,3 @@
-import { Negociacao } from "./models/negociacao.js"
-
-const negociacao = new Negociacao(new Date(), 10, 100);
+import { Negociacao } from "./models/negociacao.js";
+const negociacao = new Negociacao(new Date(), 100);
 console.log(negociacao);
